1f75a144acfe34095471ac175318d7e8ed5cee71
magpiedin <magpiedin@gmail.com>
2023-02-12 15:43:24 -0600

    cleanup env example-vars

--- tools/csv_to_json/ltc_json_validator.py
+++ tools/csv_to_json/ltc_json_validator.py
@@ -1,11 +1,11 @@
 '''Validate an LtC JSON record'''
 
 import dotenv
-import json
 import jsonschema
 import requests
 
 def main():
+    '''Validate an LtC JSON record against an LtC JSON Schema'''
 
     # Get env variables/input
     config = dotenv.dotenv_values(".env")
@@ -13,22 +13,17 @@ def main():
     schema_json_url = config['SCHEMA_JSON_URL']
 
 
+    # Retrieve JSON
     input_json = requests.request(
         "GET",
         input_json_url).json()
-        # "https://raw.githubusercontent.com/jbstatgen/dissco_registry/main/LtCProfile_RecordLevel_20230203.json?token=GHSAT0AAAAAAB6JVHBRNELMBSZIY625POVKY65FPHQ").json()
-        # https://raw.githubusercontent.com/tdwg/cd/review/examples/implementation%20examples/json/nhm_uk_json_example.json
 
     schema_json = requests.request(
         "GET",
         schema_json_url).json()
-        # "https://raw.githubusercontent.com/jbstatgen/dissco_registry/main/LtCProfile_RecordLevel_20230203.json?token=GHSAT0AAAAAAB6JVHBRNELMBSZIY625POVKY65FPHQ").json()
 
 
-    # ltc_record = json.load(input_json) # open(input_json, 'r', encoding='utf-8'))
-
-    # ltc_schema = json.load(schema_json)  # open(schema_json, 'r', encoding='utf-8'))
-
+    # Validate record-instance against schema
     print(jsonschema.validate(instance=input_json, schema=schema_json))
 
 
